2e851d42b639f8a15961bb8d74679821170f1a74
Ruichun <ruichun.ma@yale.edu>
2021-05-15 03:37:37 +0800

    part 1 done

--- src/main.rs
+++ src/main.rs
@@ -11,41 +11,36 @@ use panic_halt as _;
 use cortex_m_semihosting::{hprintln, hprint};
 
 static TASK_ID: u32 = 1;
-
+static SYSCALL_ARG_ADDR: usize = 0x2000_ff00;
  
 unsafe extern "C" fn asm_fn() {
-    static mut COUNT: usize = 0;
-    static mut TEST: usize = 42;
-    COUNT += 1;
-    // two pointers to msp
-    let tmp_ptr = 0x20013F00 as *mut u8;
-    *tmp_ptr = ((COUNT + TEST) % 256) as u8;
+    let tmp_ptr = SYSCALL_ARG_ADDR as *mut u8;
+    *tmp_ptr = 0;   // assign syscall arg
     asm!(
         "SVC  0x0",
     )
 }
 
-pub fn do_something (num: u32) -> u32 {
-    return num + 1;
+fn delay_ms (ms: usize) {
+    // hand crafted loop count
+    // make is close to accurate
+    for _ in 0..ms * 80 {
+        unsafe {
+            asm!("nop");
+        }
+    }
 }
 
 #[no_mangle]
 // #[link_section = ".example_section"]
 // #[export_name = "exported_symbol_name"]
 pub extern "C" fn task_main() -> ! {
-    let mut num: u32 = 1;
     // can not print varaiables for some reason.
     // hprintln!("Hello, world! from task #{}, num  = {}", TASK_ID, num).unwrap();
     loop {
-        if num % 2 == 0 {
-            hprint!("T").unwrap();
-        }
-        else {
-            hprint!("t").unwrap();
-        }
-        num = do_something(num);
         unsafe{
             asm_fn();
         }
+        delay_ms(250);
     }
 }
